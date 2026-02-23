962819c4a87625fba0ef8b80144073344af5154c
mermel21 <sb6760259109@lru.ac.th>
2026-02-23 12:26:10 +0700

    Update model.py

--- model.py
+++ model.py
@@ -461,3 +461,28 @@ def get_borrow_report(
     conn.close()
 
     return df
+# =========================
+# Admin / User
+# =========================
+def is_username_exists(username: str) -> bool:
+    conn = get_connection()
+    c = conn.cursor()
+
+    c.execute("SELECT 1 FROM users WHERE username = ?", (username,))
+    result = c.fetchone()
+
+    conn.close()
+    return result is not None
+
+
+def add_user(username: str, password_hash: str, role: str, is_active: int):
+    conn = get_connection()
+    c = conn.cursor()
+
+    c.execute("""
+        INSERT INTO users (username, password_hash, role, is_active)
+        VALUES (?, ?, ?, ?)
+    """, (username, password_hash, role, is_active))
+
+    conn.commit()
+    conn.close()
